a9132b5156cd54a3dd65465a184574d823b519a3
JianlingLiu <jil096@ucsd.edu>
2017-02-09 17:33:40 -0800

    almost done

---
+++ js/submit.js
@@ -0,0 +1,5 @@
+$("#popB").click(function(event){
+  alert('Messages has been sent');
+  //$("#message").append("<button class=\"mybtn btn btn-default\"><a href=\"index.html\">Back to Homepage</a></button>");
+  //event.preventDefault();
+});
